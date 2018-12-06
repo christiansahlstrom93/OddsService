dbf84fc85a5d9f19a0a11dd738642249ee355fc3
Christian Sahlström <christian.sahlstrom93@gmail.com>
2018-12-06 15:18:54 +0100

    Add Dockerfile from stiff

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-ARG JAR_FILE
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+FROM maven:3.6.0-jdk-8-alpine
+RUN mkdir -p /oddsservice
+ADD . /oddsservice
+WORKDIR /oddsservice
+RUN mvn clean compile package
+RUN ls /oddsservice/target
+FROM maven:3.6.0-jdk-8-alpine
+RUN mkdir -p /oddsservice
+WORKDIR /oddsservice
+COPY --from=0 /oddsservice/target/demo-0.0.1-SNAPSHOT.jar app.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/oddsservice/app.jar"]
--- src/main/java/com/fuzz/oddsservice/service/OddsServiceApplication.java
+++ src/main/java/com/fuzz/oddsservice/service/OddsServiceApplication.java
@@ -2,10 +2,24 @@ package com.fuzz.oddsservice.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class OddsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OddsServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
